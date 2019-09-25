3406576683cc62cf89584c0af4c23c7f5af2fe79
Prince Charles <princecharles35@yahoo.com>
2019-09-25 13:45:03 +0000

    Automatically backed up by Learn

---
+++ spec/hashketball_spec.rb
@@ -0,0 +1,10 @@
+ player_stats("Alan Anderson")
+  => { :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      }
